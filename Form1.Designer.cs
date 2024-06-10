c99c034df6b7a39e1a742124c05dff968064f80a
tien thanh <tienthanhdz000@gmail.com>
2024-06-10 16:48:31 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnam = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbsdanhsach = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtnam
+            // 
+            this.txtnam.Location = new System.Drawing.Point(119, 158);
+            this.txtnam.Name = "txtnam";
+            this.txtnam.Size = new System.Drawing.Size(138, 20);
+            this.txtnam.TabIndex = 0;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(119, 210);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(138, 20);
+            this.txtkq.TabIndex = 0;
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(144, 288);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(75, 23);
+            this.btntinh.TabIndex = 1;
+            this.btntinh.Text = "tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "năm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "kết quả";
+            // 
+            // lbsdanhsach
+            // 
+            this.lbsdanhsach.FormattingEnabled = true;
+            this.lbsdanhsach.Location = new System.Drawing.Point(412, 6);
+            this.lbsdanhsach.Name = "lbsdanhsach";
+            this.lbsdanhsach.Size = new System.Drawing.Size(376, 433);
+            this.lbsdanhsach.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 26.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(97, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 43);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tính Can Chi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbsdanhsach);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtnam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnam;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbsdanhsach;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            string Can;
+            switch (Convert.ToInt32(txtnam) % 10)
+            {
+                case 0:
+                    Can = "canh";
+                    break;
+                case 1:
+                    Can = "tân";
+                    break;
+                case 2:
+                    Can = "nhâm";
+                    break;
+                case 3:
+                    Can = "quý";
+                    break;
+                case 4:
+                    Can = "giáp";
+                    break;
+                case 5:
+                    Can = "ất";
+                    break;
+                case 6:
+                    Can = "bính";
+                    break;
+                case 7:
+                    Can = "đinh";
+                    break;
+                case 8:
+                    Can = "mậu";
+                    break;
+                case 9:
+                    Can = "kỷ";
+                    break;
+            }
+            string Chi = "";
+            switch (Convert.ToInt32(txtnam) % 12)
+            {
+                case 0:
+                    Chi = "thân";
+                    break;
+                case 1:
+                    Chi = "dậu";
+                    break;
+                case 2:
+                    Chi = "tuất";
+                    break;
+                case 3:
+                    Chi = "hợi";
+                    break;
+                case 4:
+                    Chi = "tý";
+                    break;
+                case 5:
+                    Chi = "sửu";
+                    break;
+                case 6:
+                    Chi = "dần";
+                    break;
+                case 7:
+                    Chi = "mão";
+                    break;
+                case 8:
+                    Chi = "thìn";
+                    break;
+                case 9:
+                    Chi = "tỵ";
+                    break;
+                case 10:
+                    Chi = "ngọ";
+                    break;
+                case 11:
+                    Chi = "mùi";
+                    break;
+            }
+            txtkq.Text = can + " " + Chi;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
